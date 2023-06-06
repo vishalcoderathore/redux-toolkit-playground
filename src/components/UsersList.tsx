ee7e8750699ba95f71a555c440fe9ee9f20722d6
Vishal Rathore <vishal.rathore@multitech.com>
2023-06-06 18:27:05 -0500

    updated user list to always show users title and button, only show loader when fetching the list. do not show loader on the full screen, just on the list.

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -21,32 +21,32 @@ const UsersList = (): ReactElement => {
     doAddUser();
   };
 
+  let content;
+
   if (isLoadingUsers) {
-    return <Skeleton times={1} className="h-10 w-full" />;
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error loading data!</div>;
+  } else {
+    content = data.map(user => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">{user.name}</div>
+        </div>
+      );
+    });
   }
-
-  if (loadingUsersError) {
-    return <div>Error loading data!</div>;
-  }
-
-  const renderedUsers = data.map(user => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">{user.name}</div>
-      </div>
-    );
-  });
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl ">Users</h1>
         <Button loading={isAddingUser} onClick={handleUserAdd}>
           + Add User
         </Button>
         {addingUsersError && 'Error creating user'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
